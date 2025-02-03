bd8c4c3c52c373a357f4496268675b43f3852790
breynerciro <bciro@unal.edu.co>
2025-02-03 12:34:04 -0500

    requeriments

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import myHotelRoutes from "./routes/my-hotels";
 import bodyParser from 'body-parser';
 import coWorkingsRoutes from './routes/coworkings';
 
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
