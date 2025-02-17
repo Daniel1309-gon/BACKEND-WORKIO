a396102d61502956f5ef43784b73f424f9f71b44
Daniel Gonzalez <dgonzalezca@unal.edu.co>
2025-02-16 19:57:09 -0500

    clear cookie

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -159,9 +159,7 @@ router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
 });
 
 router.post("/logout", (req: Request, res: Response) => {
-  res.cookie("auth_token", "", {
-    expires: new Date(0),
-  });
+  res.clearCookie("auth_token", { sameSite: "none", secure: true });
   res.send();
 });
 
