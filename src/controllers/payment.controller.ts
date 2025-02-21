bc8b6344c0ee9d5d53b2a5e933073047917c28e5
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2025-02-20 23:58:18 -0500

    Add files via upload

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -111,6 +111,46 @@ export const createOrder = async (req: Request, res: Response) => {
   }
 };
 
+
+//Intentando otra ruta de pago
+/* router.post(
+  "/:hotelId/bookings/payment-intent",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    const { numberOfNights } = req.body;
+    const hotelId = req.params.hotelId;
+
+    const hotel = await Hotel.findById(hotelId);
+    if (!hotel) {
+      return res.status(400).json({ message: "Hotel not found" });
+    }
+
+    const totalCost = hotel.pricePerNight * numberOfNights;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: totalCost * 100,
+      currency: "gbp",
+      metadata: {
+        hotelId,
+        userId: req.userId,
+      },
+    });
+
+    if (!paymentIntent.client_secret) {
+      return res.status(500).json({ message: "Error creating payment intent" });
+    }
+
+    const response = {
+      paymentIntentId: paymentIntent.id,
+      clientSecret: paymentIntent.client_secret.toString(),
+      totalCost,
+    };
+
+    res.send(response);
+  }
+);
+ */
+
 export const successPayment = async (req: Request, res: Response) => {
   try {
     const data = req.query;
@@ -127,7 +167,7 @@ export const successPayment = async (req: Request, res: Response) => {
       userId: number;
     };
 
-    //*Procesar el estado del pago en la base de datos
+    //Procesar el estado del pago en la base de datos
     const externalReference = JSON.parse(data.external_reference as string);
     const valoresQuery = [
       externalReference.idUsuario,
