e2a9ad6e48944520723bc65db4337761f53a8094
jaim3s <jujaimesc@unal.edu.co>
2025-02-21 21:33:35 -0500

    Remover importaciones innecesarias

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import User from "../models/user";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
 import verifyToken from "../middleware/auth";
