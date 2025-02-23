1739992162c7afc153c39727b9f3cc5ca77f021b
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2025-02-22 21:43:45 -0500

    Actualizando controlador Stripe

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -145,6 +145,41 @@ export const createOrder = async (req: Request, res: Response) => {
     res.send(response);
   }
 );
+
+router.post(
+  "/:hotelId/bookings/payment-intentStripe",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    const { numberOfNights } = req.body;
+    const hotelId = req.params.hotelId;
+
+    const hotel = await Hotel.findById(hotelId);
+    if (!hotel) {
+      return res.status(400).json({ message: "cOWROKING not found" });
+    }
+
+    const totalCost = hotel.pricePerNight * numberOfNights;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: totalCost * 100,
+      currency: "gbp",
+      metadata: {
+        hotelId,
+        userId: req.userId,
+      },
+    });
+
+    const response = {
+      paymentIntentId: paymentIntent.id,
+      clientSecret: paymentIntent.client_secret.toString(),
+      totalCost,
+    };
+
+    res.send(response);
+  }
+);
+
+
  */
 
 export const successPayment = async (req: Request, res: Response) => {
