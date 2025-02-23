ad9c4da6550e318af8f4fd3192af67ca684f0ee2
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2025-02-22 21:09:55 -0500

    Prueba Problema Token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -10,6 +10,24 @@ declare global {
   }
 }
 
+/* Prueba de problema token
+
+const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
+  const token = req.cookies["auth_token"];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
+    req.role = (decoded as JwtPayload).role;
+    next();
+  } catch (error) {
+
+    return res.status(401);
+  }
+};
+
+
+*/
+
 const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
   const token = req.cookies["auth_token"];
   if (!token) {
@@ -26,19 +44,5 @@ const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
     return res.status(401).json({ message: "unauthorized" });
   }
 };
-
-// Proband Funcionamiento Token?
-/*
-const verifyToken = (req: Request, res: Response, next: NextFunction): any => {
-  const token = req.cookies["auth_token"];
-  if (!token) {
-    return res.status(401).json({ message: "unauthorized" });
-  }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-  req.userId = (decoded as JwtPayload).userId;
-  req.role = (decoded as JwtPayload).role;
-  next();
-};
-*/
 
 export default verifyToken;
