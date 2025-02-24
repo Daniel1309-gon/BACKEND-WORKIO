aeb25b32ae79725c17c0a027d73e7d8619dc6c04
joalvarado27 <146464326+joalvarado27@users.noreply.github.com>
2025-02-23 23:10:50 -0500

    Modificacion de Admin

--- src/middleware/verify-admin.ts
+++ src/middleware/verify-admin.ts
@@ -13,6 +13,8 @@ const verifyAdmin = (req: Request, res: Response, next: NextFunction): void => {
 
 const verifyAdminGeneral = (req: Request, res: Response, next: NextFunction): void => {
 if (!req.userId || req.role !== "admin") {
+ res.status(403).json({ message: "Access denied: Admins only" });
+    return;
   }
   next();
 };
